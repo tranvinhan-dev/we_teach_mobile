1ca1cde05e94000062a9ed5202e86a3bc90d0844
tranvinhan <tranvinhan.coder@gmail.com>
2021-10-05 19:56:36 +0700

    add student meeting

---
+++ Student/Weteach/app/src/main/java/com/example/weteach/MeetTeacherActivity.java
@@ -0,0 +1,14 @@
+package com.example.weteach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeetTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meet_teacher);
+    }
+}
