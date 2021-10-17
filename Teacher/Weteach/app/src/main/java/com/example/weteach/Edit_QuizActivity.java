61b4e43f3dcdf0907033594be2d324b2df90864e
Minh Nguyen <minh250400@gmail.com>
2021-10-17 14:06:21 +0700

    adjust create quiz layout

---
+++ Teacher/Weteach/app/src/main/java/com/example/weteach/Edit_QuizActivity.java
@@ -0,0 +1,14 @@
+package com.example.weteach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_quiz);
+    }
+}
