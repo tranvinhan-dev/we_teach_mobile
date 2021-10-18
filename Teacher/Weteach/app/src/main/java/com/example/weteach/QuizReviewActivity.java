d6004377a01ded7535f72be68676eae940dd92c7
Minh Nguyen <minh250400@gmail.com>
2021-10-18 23:06:22 +0700

    quiz review

---
+++ Teacher/Weteach/app/src/main/java/com/example/weteach/QuizReviewActivity.java
@@ -0,0 +1,14 @@
+package com.example.weteach;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_review);
+    }
+}
